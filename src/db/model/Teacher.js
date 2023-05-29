dc6902bbc7b9a2b32454a00d7972174ab77c058c
Ítalo Alves <italooko@gmail.com>
2023-05-29 20:33:18 -0300

    feat: undo teacher image column and adding to common user

--- src/db/model/Teacher.js
+++ src/db/model/Teacher.js
@@ -17,9 +17,6 @@ const Teacher = database.define('Teacher', {
         type: DataTypes.STRING,
         allowNull: false,
         primaryKey: true
-    },
-    image: {
-        type: DataTypes.TEXT,
     }
 }, {
     freezeTableName: true
