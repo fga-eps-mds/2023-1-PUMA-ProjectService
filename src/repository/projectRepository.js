57ad472619f430f5959fbb4fa85400829d7cf984
artmarmocathala <arthurmarmocathala@gmail.com>
2023-07-06 19:14:42 -0700

    fix: reflect changes to Project model into projectRepository
    Co-authored-by: Joao Pedro <joaopedroveras310103@gmail.com>
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>

--- src/repository/projectRepository.js
+++ src/repository/projectRepository.js
@@ -123,6 +123,7 @@ module.exports = {
         status: project.status,
         image: project.projectImage,
         pdf: project.projectPdf,
+        isBestProject: project.isBestProject,
       }).then((response) => {
         resolve(response);
       }).catch((error) => {
@@ -143,6 +144,7 @@ module.exports = {
       const status = project.status;
       const image = project.projectImage; 
       const pdf = project.projectPdf; 
+      const isBestProject = project.isBestProject;
       Project.update({
         subjectId,
         name,
@@ -152,6 +154,7 @@ module.exports = {
         status,
         image,
         pdf,
+        isBestProject,
       }, {
         where: {
           projectId,
@@ -177,6 +180,7 @@ module.exports = {
     return new Promise((resolve, reject) => {
       Project.update({
         deleted: true,
+        isBestProject: false,
       }, {
         where: {
           projectId,
